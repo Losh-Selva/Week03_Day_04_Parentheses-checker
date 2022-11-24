d94b66955240400b91b93bff9e73afd630627c9a
Loshanth Selvanayagam <loshanth75@googlemail.com>
2022-11-24 18:54:41 +0000

    code with extensions

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,74 @@
+import java.util.Stack;
+
+public class ParenthesesChecker {
+    public static void main(String[] args) {
+        System.out.println(checkParentheses("test braces}"));
+
+    }
+
+
+        public static boolean checkParentheses (String string) {
+            Stack<String> checkStack = new Stack<>();
+            int openBracketCounter= 0;
+            int closedBracketCounter= 0;
+        for (int i = 0; i < string.length(); i++){
+            //MVP METHOD
+           /* if (String.valueOf(string.charAt(i)).equals("(")
+                    || String.valueOf(string.charAt(i)).equals("{")
+                    || String.valueOf(string.charAt(i)).equals("[")
+                    || String.valueOf(string.charAt(i)).equals("<")){
+                openBracketCounter++;
+            } else if (String.valueOf(string.charAt(i)).equals(")")
+                    || String.valueOf(string.charAt(i)).equals("}")
+                    || String.valueOf(string.charAt(i)).equals("]")
+                    || String.valueOf(string.charAt(i)).equals(">")) {
+                closedBracketCounter++;
+            }*/
+            switch (String.valueOf(string.charAt(i))){
+                case "(" :
+                    checkStack.push(")");
+                    break;
+                case "{":
+                    checkStack.push("}");
+                    break;
+                case "<" :
+                    checkStack.push(">");
+                    break;
+                case "[" :
+                    checkStack.push("]");
+                    break;
+                case ")" :
+                    if( checkStack.size() ==0) {
+                        checkStack.push("]") ;
+                    } else if (checkStack.peek() == ")") {
+                        checkStack.pop();;
+                    }
+                    break;
+                case "}" :
+                    if( checkStack.size() ==0) {
+                        checkStack.push("]") ;
+                    } else if (checkStack.peek() == "}") {
+                        checkStack.pop();;
+                    }
+                    break;
+                case ">" :
+                    if( checkStack.size() ==0) {
+                        checkStack.push("]") ;
+                    } else if (checkStack.peek() == ">") {
+                        checkStack.pop();;
+                    }
+                    break;
+                case "]" :
+                    if( checkStack.size() ==0) {
+                        checkStack.push("]") ;
+                    } else if (checkStack.peek() == "]") {
+                        checkStack.pop();;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return checkStack.size() == 0;
+    }
+}
